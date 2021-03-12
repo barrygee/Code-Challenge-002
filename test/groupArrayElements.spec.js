ba708b6e436e2c7dc251bfab628cb91e58d96bf2
Barry <barrymarkgee@gmail.com>
2021-03-12 07:40:54 +0000

    Add additional unit test to cover invalid array and groupSize values

--- test/groupArrayElements.spec.js
+++ test/groupArrayElements.spec.js
@@ -8,20 +8,56 @@ describe('groupArrayElements', () => {
         expect(() => groupArrayElements(undefined, 5)).to.throw(Error, 'An array is required');
     });
 
-    it('should throw an error when a groupSize value is not provided', () => {
-        expect(() => groupArrayElements([1,2,3,4,5,6,7,8,9], undefined)).to.throw(Error, 'An invalid groupSize value was provided');
+    it('should throw an error when an undefined value is provided instead of an Array', () => {
+        expect(() => groupArrayElements(undefined, 2)).to.throw(Error, 'An array is required');
     });
 
-    it('should throw an error when an invalid groupSize value is provided', () => {
-        expect(() => groupArrayElements([1,2,3,4,5,6,7,8,9], -1)).to.throw(Error, 'An invalid groupSize value was provided');
+    it('should throw an error when a negative value is provided instead of an Array', () => {
+        expect(() => groupArrayElements(-1, 2)).to.throw(Error, 'An array is required');
     });
 
-    it('should throw an error when an invalid groupSize value is provided', () => {
-        expect(() => groupArrayElements([1,2,3,4,5,6,7,8,9], 'a')).to.throw(Error, 'An invalid groupSize value was provided');
+    it('should throw an error when a string value is provided instead of an Array', () => {
+        expect(() => groupArrayElements('a', 2)).to.throw(Error, 'An array is required');
+    });
+
+    it('should throw an error when a null value is provided instead of an Array', () => {
+        expect(() => groupArrayElements(null, 2)).to.throw(Error, 'An array is required');
+    });
+
+    it('should throw an error when a boolean true value is provided instead of an Array', () => {
+        expect(() => groupArrayElements(true, 2)).to.throw(Error, 'An array is required');
+    });
+
+    it('should throw an error when a boolean false value is provided instead of an Array', () => {
+        expect(() => groupArrayElements(false, 2)).to.throw(Error, 'An array is required');
     });
 
     it('should return an empty Array when an Empty Array is provided ', () => {
         expect(groupArrayElements([], 10)).to.deep.equal([]);
+    });
+
+    it('should throw an error when an undefined groupSize value is provided', () => {
+        expect(() => groupArrayElements([1,2,3,4,5,6,7,8,9], undefined)).to.throw(Error, 'An invalid groupSize value was provided');
+    });
+
+    it('should throw an error when a negative groupSize value is provided', () => {
+        expect(() => groupArrayElements([1,2,3,4,5,6,7,8,9], -1)).to.throw(Error, 'An invalid groupSize value was provided');
+    });
+
+    it('should throw an error when a string groupSize value is provided', () => {
+        expect(() => groupArrayElements([1,2,3,4,5,6,7,8,9], 'a')).to.throw(Error, 'An invalid groupSize value was provided');
+    });
+
+    it('should throw an error when a null groupSize value is provided', () => {
+        expect(() => groupArrayElements([1,2,3,4,5,6,7,8,9], null)).to.throw(Error, 'An invalid groupSize value was provided');
+    });
+
+    it('should throw an error when a boolean true groupSize value is provided', () => {
+        expect(() => groupArrayElements([1,2,3,4,5,6,7,8,9], true)).to.throw(Error, 'An invalid groupSize value was provided');
+    });
+
+    it('should throw an error when a boolean false groupSize value is provided', () => {
+        expect(() => groupArrayElements([1,2,3,4,5,6,7,8,9], false)).to.throw(Error, 'An invalid groupSize value was provided');
     });
 
     it('should return a single nested Array containing all values when the groupSize value is larger than the array length', () => {
